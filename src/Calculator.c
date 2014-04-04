7bdf218dfa1cef50c069b1af154747215c879d0b
Soo Fatt <soofatt93@gmail.com>
2014-04-04 14:46:08 +0800

    Add tests for evaluateAllOperatorsOnStack

--- src/Calculator.c
+++ src/Calculator.c
@@ -7,7 +7,28 @@
 #include "Calculator.h"
 
 void tryEvaluateOperatorsOnStackThenPush(Stack *operatorStack, Stack *dataStack, OperatorToken *operator){
+	OperatorToken *opToken;
 	
+	opToken = pop(operatorStack);
+	
+	if(opToken->precedence >= operator->precedence){
+		evaluateOperator(dataStack, opToken);
+		push(operatorStack, operator);
+	}
+	else
+		push(operatorStack, operator);
+	
+}
+
+void evaluateAllOperatorsOnStack(Stack *operatorStack, Stack *dataStack){
+	OperatorToken *opToken;
+	
+	opToken = pop(operatorStack);
+	
+	while(opToken != NULL){
+		evaluateOperator(dataStack, opToken);
+		opToken = pop(operatorStack);
+	}
 }
 
 void evaluateOperator(Stack *dataStack, OperatorToken *operator){
--- test/test_Calculator.c
+++ test/test_Calculator.c
@@ -4,11 +4,35 @@
 #include "mock_Stack.h"
 #include "Error.h"
 #include "Calculator.h"
+#include <stdio.h>
 
 void setUp(){}
 void tearDown(){}
 
-void test_tryEvaluateOperatorsOnStackThenPush_should_evaluate_mul_and_plus(){
+void test_tryEvaluateOperatorsOnStackThenPush_should_evaluate_mul_first_then_push_minus(){
+	Stack dataStack;
+	Stack operatorStack;
+	OperatorToken minus = {.type = OPERATOR_TOKEN, .name = "-", .precedence = 70};
+	OperatorToken multiply = {.type = OPERATOR_TOKEN, .name = "*", .precedence = 100};
+	NumberToken num2 = {.type = NUMBER_TOKEN, .value = 2};
+	NumberToken num10 = {.type = NUMBER_TOKEN, .value = 10};
+	NumberToken num4 = {.type = NUMBER_TOKEN, .value = 4};
+	NumberToken num20 = {.type = NUMBER_TOKEN, .value = 20};
+	NumberToken num16 = {.type = NUMBER_TOKEN, .value = 16};
+	
+	//2 * 10 - 4 = 16
+	pop_ExpectAndReturn(&operatorStack, &multiply);
+	pop_ExpectAndReturn(&dataStack, &num2);
+	pop_ExpectAndReturn(&dataStack, &num10);
+	createNumberToken_ExpectAndReturn(20, &num20);
+	push_Expect(&dataStack, &num20);
+	push_Expect(&operatorStack, &minus);
+	
+	tryEvaluateOperatorsOnStackThenPush(&operatorStack, &dataStack, &minus);
+	
+}
+
+void test_evaluateAllOperatorsOnStack_should_evaluate_mul_and_plus(){
 	Stack dataStack;
 	Stack operatorStack;
 	OperatorToken plus = {.type = OPERATOR_TOKEN, .name = "+", .precedence = 70};
@@ -16,13 +40,51 @@ void test_tryEvaluateOperatorsOnStackThenPush_should_evaluate_mul_and_plus(){
 	NumberToken num2 = {.type = NUMBER_TOKEN, .value = 2};
 	NumberToken num3 = {.type = NUMBER_TOKEN, .value = 3};
 	NumberToken num5 = {.type = NUMBER_TOKEN, .value = 5};
+	NumberToken num15 = {.type = NUMBER_TOKEN, .value = 15};
+	NumberToken num17 = {.type = NUMBER_TOKEN, .value = 17};
 	
-	pop_ExpectAndReturn(&dataStack, &num2);
+	//2 + 3 * 5 = 17
+	pop_ExpectAndReturn(&operatorStack, &multiply);
 	pop_ExpectAndReturn(&dataStack, &num3);
-	createNumberToken_ExpectAndReturn(5, &num5);
-	push_Expect(&dataStack, &num5);
+	pop_ExpectAndReturn(&dataStack, &num5);
+	createNumberToken_ExpectAndReturn(15, &num15);
+	push_Expect(&dataStack, &num15);
+	pop_ExpectAndReturn(&operatorStack, &plus);
+	pop_ExpectAndReturn(&dataStack, &num15);
+	pop_ExpectAndReturn(&dataStack, &num2);
+	createNumberToken_ExpectAndReturn(17, &num17);
+	push_Expect(&dataStack, &num17);
+	pop_ExpectAndReturn(&operatorStack, NULL);
 	
-	evaluateOperator(&dataStack, &plus);
+	evaluateAllOperatorsOnStack(&operatorStack, &dataStack);
+	
+}
+
+void test_evaluateAllOperatorsOnStack_should_evaluate_xor_and_or(){
+	Stack dataStack;
+	Stack operatorStack;
+	OperatorToken xor = {.type = OPERATOR_TOKEN, .name = "^", .precedence = 50};
+	OperatorToken or = {.type = OPERATOR_TOKEN, .name = "|", .precedence = 10};
+	NumberToken num5 = {.type = NUMBER_TOKEN, .value = 5};
+	NumberToken num6 = {.type = NUMBER_TOKEN, .value = 6};
+	NumberToken num4 = {.type = NUMBER_TOKEN, .value = 4};
+	NumberToken num3 = {.type = NUMBER_TOKEN, .value = 3};
+	NumberToken num7 = {.type = NUMBER_TOKEN, .value = 7};
+	
+	//5 ^ 6 | 4 = 7
+	pop_ExpectAndReturn(&operatorStack, &xor);
+	pop_ExpectAndReturn(&dataStack, &num5);
+	pop_ExpectAndReturn(&dataStack, &num6);
+	createNumberToken_ExpectAndReturn(3, &num3);
+	push_Expect(&dataStack, &num3);
+	pop_ExpectAndReturn(&operatorStack, &or);
+	pop_ExpectAndReturn(&dataStack, &num3);
+	pop_ExpectAndReturn(&dataStack, &num4);
+	createNumberToken_ExpectAndReturn(7, &num7);
+	push_Expect(&dataStack, &num7);
+	pop_ExpectAndReturn(&operatorStack, NULL);
+	
+	evaluateAllOperatorsOnStack(&operatorStack, &dataStack);
 	
 }
 
